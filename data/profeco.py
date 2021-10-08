cd78ce7710c24a1797cfc96fecb7783c4a079d40
MiguelRodriguezSayeg <41605911+MiguelRodriguezSayeg@users.noreply.github.com>
2021-10-07 20:34:43 -0500

    Refactor: fixed minor bugs on large scale file.
    Fixed missing character error and error on groupby clause.

--- data/profeco.py
+++ data/profeco.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-
-# file_name = "all_data.csv"
-file_name = "sample.csv"
+file_name = "all_data.csv"
+#file_name = "sample.csv"
 chunk_size = 10 ** 6
 
 commercial_set = set()
@@ -21,24 +20,26 @@ for chunk in pd.read_csv(file_name, chunksize=chunk_size):
     commercial_set.update(chunk['cadenaComercial'].drop_duplicates().tolist())
 
     # Calculating the top 10 products per state.
-    a = chunk.groupby('estado')['producto'].apply(lambda x: x.value_counts())
-    top_10_frame = a.to_frame()
-    if top_10:
+    top_10_frame = chunk.groupby('estado')['producto'].apply(lambda x: x.value_counts())
+    if top_10 is not None:
         top_10 = top_10.add(top_10_frame, fill_value=0)
     else:
         top_10 = top_10_frame
 
-    # Calculating the top 10 products per state.
-    b = chunk.groupby('cadenaComercial').size()
-    if top_chain:
-        top_chain = top_chain.add(b, fill_value=0)
+    # Calculating the commercial chain with the most monitored
+    # products
+    chain = chunk.groupby('cadenaComercial').size()
+    if top_chain is not None:
+        top_chain = top_chain.add(chain, fill_value=0)
     else:
-        top_chain = b
+        top_chain = chain
 
     # Calculating the mean product price for each company on the same
     # business line products.
+    chunk['precio'] = pd.to_numeric(chunk['precio'], errors='coerce').fillna(0)
     c = chunk.groupby(['giro', 'cadenaComercial'])['precio'].mean()
-    if mean_cost:
+
+    if mean_cost is not None:
         mean_cost = mean_cost.add(c, fill_value=0)
     else:
         mean_cost = c
@@ -48,10 +49,11 @@ print("DIFFERENT COMMERCIAL CHAINS COUNTED:")
 print(len(commercial_set))
 
 print("TOP 10 PRODUCTS PER MEXICAN STATE:")
-print(top_10.groupby('estado').head(10).to_string())
+top_10 = top_10.to_frame()
+print(top_10.sort_values(['estado', top_10.columns[-1]], ascending=False).groupby('estado').head(10))
 
 print("CHAIN WITH MOST MONITORED PRODUCTS:")
 print(top_chain.sort_values(ascending=False).head(1).to_string())
 
-print("BUSINESS LINE AND AVERAGE COST PER COMMERCIAL CHAIN:")
-print(mean_cost.to_frame().sort_values(['giro','precio'], ascending=True))
+print("LEAST AVERAGE COST PER COMMERCIAL CHAIN IN SAME BUSINESS LINE:")
+print(mean_cost.to_frame().sort_values(['giro','precio'], ascending=True).head(1))
